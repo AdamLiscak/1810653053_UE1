b4a2935bf9dc297cd3be26cb104dcc42b6627d92
Adam liscak <someone@someplace.com>
2018-11-13 17:29:01 +0100

    Aufgabe_3_Commit_1

---
+++ src/Augabe3_1810653053.java
@@ -0,0 +1,16 @@
+public class Augabe3_1810653053
+{
+    public static void main (String[] args)
+    {
+      int zahl1= 6;
+      int zahl2= 2;
+      int zahl3= 3;
+
+      double zahl4= 4;
+      double zahl5= 5;
+      double zahl6= 6;
+
+
+    }
+
+}
