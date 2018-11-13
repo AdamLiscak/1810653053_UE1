41f190543a884dcd64c077184d6cca645070c539
Adam liscak <someone@someplace.com>
2018-11-13 17:08:22 +0100

    Aufgab2

---
+++ src/Aufgab2_1810653053.java
@@ -0,0 +1,29 @@
+public class Aufgab2_1810653053
+{
+    public static void main(String []args )
+    {
+      String txt= "txt" ; // wert: "txt" ; Wertebereich: 2147483647 ((2^31) - 1) chars 32 bit
+      int five= 5; // wert: "5" ; Wertebereich: -2147483648 bis +2147483647 (-2^31 bis 2^31) 32 bit
+
+      double thousandE= 2.718281828459045e3; // wert: "1000*2.718281828459045" ; Wertebereich: -1.7E308 bis +1.7E308 64 bit
+      boolean falsch = false ; // wert: "false" ; Wertebereich: true/false
+
+      long Quazillion= 1000000000000000000L; /* wert: "10000000000000000" ;
+      Wertebereich: -9,223,372,036,854,775,808 bis 9,223,372,036,854,775,807 (2^-63 bis (2^63)-1) 64 bit */
+
+      char k = 'k'; // wert: "k" ; Wertebereich: 1 char
+
+      byte minecraftHungerScore = 127; // wert: "127" ; Wertebereich: -128 bis 127 (-2^7 bis (2^7)-1) 8 bit
+
+      short currentPosition= -32768; // wert: "-32768" ; Wertebereich: -32768 bis 32767 (-2^15 bis (2^15)-1 )16 bit
+
+      float ooEShort=2.71828e-3f; // wert: "2.71828" ; Wertebereich: -3.4E38 bis +3.4E38 32 bit
+
+      final int KONSTANTE= 218; /* wert: "218" ; Wertebereich: -2147483648 bis +2147483647 (-2^31 bis 2^31)
+      (-2^31 bis 2^31) 32 bit */
+
+      /* Eine Konstante kann man nach der Deklaration in der Lebensdauer des Objektes nicht mehr verändern.
+      Eine Variable kann vom User oder Programm verändert werden.
+       */
+    }
+}
