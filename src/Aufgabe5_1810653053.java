704cde58b71f65eabe3084f72292ee65e9a6da18
Adam liscak <adam.liscak@live.com>
2018-11-13 20:25:26 +0100

    Aufgabe5

---
+++ src/Aufgabe5_1810653053.java
@@ -0,0 +1,13 @@
+public class Aufgabe5_1810653053 {
+    public static void main ( String[] args)
+    {
+        int zahl1=1;
+        int zahl2= 3;
+        zahl1++;
+        zahl2++;
+        System.out.println("zahl1++: "+zahl1+" zahl2++: "+zahl2);
+        ++zahl1;
+        ++zahl2;
+        System.out.println("++zahl1: "+zahl1+" ++zahl2: "+zahl2);
+    }
+}
