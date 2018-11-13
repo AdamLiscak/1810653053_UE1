8abdd0e2ea8d7d76dd39213bbe7b04f9ae213383
Adam liscak <adam.liscak@live.com>
2018-11-13 20:16:35 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653053.java
@@ -0,0 +1,20 @@
+public class Aufgabe4_1810653053
+{
+    public static void main(String[]args)
+    {
+        int zahl1 = 3;
+        int zahl2 = 5;
+        System.out.println("zahl1= "+zahl1+" zahl2= "+zahl2);
+        System.out.println("zahl1 kleiner zahl2: " + (zahl1<zahl2));
+        System.out.println("zahl2 kleiner zahl1: " + (zahl1>zahl2));
+        System.out.println("zahl1 größer zahl2: " + (zahl1>zahl2));
+        System.out.println("zahl2 größer zahl1: " + (zahl1<zahl2));
+        System.out.println("zahl1 kleineroder gleich zahl2: "+(zahl1<=zahl2));
+        System.out.println("zahl2 kleineroder gleich zahl1: "+(zahl1>=zahl2));
+        System.out.println("zahl1 größer oder gleich zahl2: " +(zahl1>=zahl2));
+        System.out.println("zahl2 größer oder gleich zahl1: "+(zahl1<=zahl2));
+        System.out.println("zahl1 gleich zahl2: "+(zahl1==zahl2));
+        System.out.println("zahl1 ungleich zahl2: "+(zahl1!=zahl2));
+
+    }
+}
