cedf1addf3b94525fc6d09209a8524646a4e9a4a
Adam liscak <adam.liscak@live.com>
2018-11-13 19:50:50 +0100

    Aufgabe3_commit_3

--- src/Augabe3_1810653053.java
+++ src/Augabe3_1810653053.java
@@ -3,13 +3,30 @@ public class Augabe3_1810653053
     public static void main (String[] args)
     {
       int zahl1= 6;
-      int zahl2= 2;
-      int zahl3= 3;
+      int zahl2= 7;
+      int zahl3= 5;
+
 
       double zahl4= 4;
       double zahl5= 5;
       double zahl6= 6;
 
+      int oneplustwo=zahl1+zahl2;
+      double onedurchdrei =  (double)zahl1/(double)zahl3;
+      double fourmaltwo = zahl4*(double)zahl2;
+      int moduloonetwo= zahl1%zahl2;
+        zahl3++;
+        zahl2--;
+
+
+
+        System.out.println("zahl1+zahl2: "+ oneplustwo );
+        System.out.println("zahl1/zahl3: " + onedurchdrei );
+        System.out.println("zahl4*zahl2: " + fourmaltwo );
+        System.out.println("zahl1 mod zahl2: "+ moduloonetwo);
+        System.out.println("zahl3 inkrementiert: " +zahl3);
+        System.out.println("zahl2 dekrementiert: "+ zahl2);
+        System.out.println("___________________________");
 
     }
 
